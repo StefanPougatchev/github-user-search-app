f158abe13368afd0ba46221e811c4544e691ed7e
StefanPougatchev <Stefan.Pougatchev@gmail.com>
2024-11-01 10:34:23 -0700

    updated readme

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -6,7 +6,7 @@ import { Octokit } from 'octokit';
 import PropTypes from 'prop-types';
 
 const octokit = new Octokit({
-	auth: 'sample',
+	auth: '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',
 });
 
 const SearchBar = (props) => {
